1b4973d835e7f9faa806efa8fca40989002f67a8
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-03-05 23:25:58 +0300

    Last Fix

--- BinarySearch/src/search/BinarySearchShift.java
+++ BinarySearch/src/search/BinarySearchShift.java
@@ -2,27 +2,27 @@ package search;
 
 public class BinarySearchShift {
         // Pre: any i: args[i] -> Integer &&
-        // exists k in [0, .. args.length] : any i, j : [0.. k) && i < j => args[i] < args[j]
+        // exists k in (0, .. args.length] : any i, j : [0.. k) && i < j => args[i] < args[j]
         // any i, j : [k, args.length) && i < j => args[i] < args[j]
-        // (k > 0 && k < args.length) => a[0] > a[k]
+        // k < args.length => a[0] > a[k]
         // Post: k in [0.. a.size())
         public static void main(String[] args) {
         int[] a = new int[args.length];
         for (int i = 0; i < args.length; i++) {
             a[i] = Integer.parseInt(args[i]);
         }
-        int r = iterativeBinarySearch(a);
-        //int r = recursiveBinarySearch(a, -1, a.size());
+        //int r = iterativeBinarySearch(a);
+        int r = recursiveBinarySearch(a, 0, a.length);
         // if k = a.length => any i, j in [0.. a.length) i < j => a[i] < a[j] => exists k' = 0
         System.out.println((r == a.length ? 0 : r));
     }
 
-    // Pre: exists k in [0, .. a.length] : any i, j : [0.. k) && i < j => a[i] < a[j]
+    // Pre: exists k in (0, .. a.length] : any i, j : [0.. k) && i < j => a[i] < a[j]
     // any i, j : [k, a.length) && i < j => a[i] < a[j]
-    // (k > 0 && k < args.length) => a[0] > a[k]
-    // Post: r = k && 0 < k <= a.size()
+    // k < args.length => a[0] > a[k]
+    // Post: R = k
     private static int iterativeBinarySearch(int[] a) {
-        int l = -1, r = a.length;
+        int l = 0, r = a.length;
         // Inv: l < k && r >= k
         while (r - l > 1) {
             // r - l > 1
@@ -47,11 +47,11 @@ public class BinarySearchShift {
         return r;
     }
 
-    // Pre: exists k in [0, .. a.length] : any i, j : [0.. k) && i < j => a[i] < a[j]
+    // Pre: exists k in (0, .. a.length] : any i, j : [0.. k) && i < j => a[i] < a[j]
     // any i, j : [k, a.length) && i < j => a[i] < a[j]
-    // (k > 0 && k < args.length) => a[0] > a[k]
-    // l < k && r >= k && l >= -1 && r <= a.size()
-    // Post: r = k && 0 < k <= a.size()
+    // k < args.length => a[0] > a[k]
+    // l < k && r >= k && l >= 0 && r <= a.size()
+    // Post: R = k
     private static int recursiveBinarySearch(int[] a, int l, int r) {
         if (r - l > 1) {
             // r - l > 1
@@ -70,7 +70,7 @@ public class BinarySearchShift {
             }
             // r' - l' < r - l
             // r > l
-            return r = recursiveBinarySearch(a, l, r);
+            return recursiveBinarySearch(a, l, r);
         } else {
             // r - l == 1
             // l < k && r >= k && r - l == 1 => r = k
