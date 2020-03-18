14aee2556dbe98e9b0c20675845b0098e9e267ba
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-03-19 00:47:57 +0400

    Delete for

--- ExpressionJS/functionalExpression.js
+++ ExpressionJS/functionalExpression.js
@@ -1,64 +1,33 @@
 "use strict";
 
-const cnst = (value) => (x) => value;
+const cnst = value => x => value;
 
-const variable = (name) => (x) => x;
+const variable = name => x => x;
 
-const operation = (op) => (a, b) => (x) => op(a(x), b(x));
-const multiply = operation((a, b) => (a * b));
-const subtract = operation((a, b) => (a - b));
-const add = operation((a, b) => (a + b));
-const divide = operation((a, b) => (a / b));
+const operation = op => (a, b) => x => op(a(x), b(x));
+const multiply = operation((a, b) => a * b);
+const subtract = operation((a, b) => a - b);
+const add = operation((a, b) => a + b);
+const divide = operation((a, b) => a / b);
+
+let symbolToOperation = {};
+symbolToOperation['+'] = add;
+symbolToOperation["-"] = subtract;
+symbolToOperation["*"] = multiply;
+symbolToOperation["/"] = divide;
 
 function parse (expression) {
     let stack = [];
-    for (let i = 0; i < expression.length; i++) {
-        while (i < expression.length && expression[i] === " ") {
-            i++;
-        }
-        if (i >= expression.length) {
-            break;
-        }
-        switch (expression[i]) {
-            case "-":
-                if (i < expression.length - 1 && isDigit(expression[i + 1])) {
-                    i++;
-                    stack.push(cnst(-parseNumber()));
-                } else {
-                    stack.push(makeOperation(stack, subtract));
-                }
-                break;
-            case "+":
-                stack.push(makeOperation(stack, add));
-                break;
-            case "*":
-                stack.push(makeOperation(stack, multiply));
-                break;
-            case "/":
-                stack.push(makeOperation(stack, divide));
-                break;
-            case "x":
-                stack.push(variable("x"));
-                break;
-            default:
-                stack.push(cnst(parseNumber()));
-        }
-        function parseNumber() {
-            let c = 0;
-            while (i < expression.length && isDigit(expression[i])) {
-                c *= 10;
-                c += +expression[i];
-                i++;
-            }
-            return c;
+    expression.split(" ").filter(s => s.trim() !== "").forEach(parseToken);
+    function parseToken(token) {
+        if (symbolToOperation[token] !== undefined) {
+            let b = stack.pop(), a = stack.pop();
+            stack.push(symbolToOperation[token](a, b));
+        } else if (token === "x") {
+            stack.push(variable(token));
+        } else {
+            stack.push(cnst(+token));
         }
     }
-    return stack[0];
+    return stack.pop();
 }
-
-function makeOperation(stack, operation){
-    let b = stack.pop(), a = stack.pop();
-    return operation(a, b);
-}
-
-const isDigit = x => x >= "0" && x <= "9";
