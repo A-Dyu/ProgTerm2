682654f7d0f5e746b129aee1f4107cd5d36502f6
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-04-14 22:31:31 +0300

    style fix

--- ClojureLinear/linear.clj
+++ ClojureLinear/linear.clj
@@ -1,16 +1,16 @@
-(defn checkArgs [f args]
-  (reduce (fn [a b] (and a b)) true (mapv f args)))
-
 (defn checkVectorsEqual [args]
-  (checkArgs (fn [x] (== (count x) (count (first args)))) args))
+  (every? (partial == (count (first args))) (mapv count args)))
+
+(defn isVector [v]
+  (and (vector? v) (every? number? v)))
 
 (defn isMatrix [m]
-  (and (vector? m) (checkArgs vector? m) (checkVectorsEqual m)))
+  (and (vector? m) (every? isVector m) (checkVectorsEqual m)))
 
 (defn funByElements [f args]
-  {:pre [(checkArgs vector? args) (checkVectorsEqual args) (checkArgs (fn [x] (= (type x) (type (first args)))) args)]}
+  {:pre [(checkVectorsEqual args)]}
     (if (== (count args) 1)
-       (mapv  f (first args))
+       (mapv f (first args))
        (reduce
           (fn [a b]
             (loop [ans [] x a y b]
@@ -20,39 +20,51 @@
 
 (defn getRow [m i] (mapv (fn [v] (nth v i)) m))
 
-(defn v+ [& args] (funByElements + args))
+(defn v+ [& args]
+  {:pre [(every? isVector args)]}
+  (funByElements + args))
 
-(defn v- [& args] (funByElements - args))
+(defn v- [& args]
+  {:pre [(every? isVector args)]}
+  (funByElements - args))
 
-(defn v* [& args] (funByElements * args))
+(defn v* [& args]
+  {:pre [(every? isVector args)]}
+  (funByElements * args))
 
 (defn v*s [v & args]
-  {:pre [(vector? v) (checkArgs number? v) (checkArgs number? args)]}
+  {:pre [(isVector v) (every? number? args)]}
   (reduce (fn [v x] (mapv (partial * x) v)) v args))
 
 (defn scalar [a b]
-  {:pre [(vector? a) (vector? b) (== (count a) (count b))]}
+  {:pre [(isVector a) (isVector b) (== (count a) (count b))]}
   (reduce + (v* a b)))
 
 (defn vect [& args]
-  {:pre [(checkArgs vector? args) (checkVectorsEqual args) (== (count (first args)) 3) (checkArgs (fn [x] (number? (first x))) args)]}
+  {:pre [(every? isVector args) (checkVectorsEqual args) (== (count (first args)) 3)]}
   (reduce (fn ([a b] (vector
    (- (* (nth a 1) (nth b 2)) (* (nth a 2) (nth b 1)))
    (- (* (nth a 2) (nth b 0)) (* (nth a 0) (nth b 2)))
    (- (* (nth a 0) (nth b 1)) (* (nth a 1) (nth b 0)))))) args))
 
-(defn m+ [& args] (funByElements v+ args))
+(defn m+ [& args]
+  {:pre [(every? isMatrix args)]}
+  (funByElements v+ args))
 
-(defn m- [& args] (funByElements v- args))
+(defn m- [& args]
+  {:pre [(every? isMatrix args)]}
+  (funByElements v- args))
 
-(defn m* [& args] (funByElements v* args))
+(defn m* [& args]
+  {:pre [(every? isMatrix args)]}
+  (funByElements v* args))
 
 (defn m*s [m & args]
-  {:pre [(isMatrix m) (checkArgs number? args)]}
+  {:pre [(isMatrix m) (every? number? args)]}
   (reduce (fn [m s] (mapv (fn [v] (v*s v s)) m)) m args))
 
 (defn m*v [m v]
-  {:pre [(isMatrix m) (vector? v) (number? (first v))]}
+  {:pre [(isMatrix m) (isVector v) (== (count (first m)) (count v))]}
   (mapv (fn [a] (reduce + (v* a v))) m))
 
 (defn transpose [m]
@@ -63,7 +75,8 @@
       (recur (conj ans (getRow m i)) (+ i 1)))))
 
 (defn m*m [& args]
-  (reduce (fn [a b] {:pre [(isMatrix a) (isMatrix b) (== (count (first a)) (count b))]}
+  (reduce (fn [a b]
+    {:pre [(isMatrix a) (isMatrix b)]}
     (letfn [
              (mGetCol [a b j]
                (loop [i 0 ans []]
