260fe408565538397cced982bdb521690eb8a2f7
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-05-01 00:07:22 +0300

    Update linear.clj

--- ClojureLinear/linear.clj
+++ ClojureLinear/linear.clj
@@ -1,64 +1,67 @@
 (defn checkVectorsEqual [args] (or (every? number? args) (and (every? vector? args) (every? (partial == (count (first args))) (mapv count args)))))
 
-(defn isVector [v] (and (vector? v) (every? number? v)))
+(defn isVector? [v] (and (vector? v) (every? number? v)))
 
-(defn isMatrix [m] (and (vector? m) (every? isVector m) (checkVectorsEqual m)))
+(defn isMatrix? [m] (and (vector? m) (every? isVector? m) (checkVectorsEqual m)))
+
+(defn isTensor? [t] (or (every? number? t) (and (checkVectorsEqual t) (every? isTensor? t))))
 
 (defn funByElements [f args checkArgs]
   {:pre [(checkVectorsEqual args) (every? checkArgs args)]}
-  (if (every? number? args) (apply f args) (apply mapv f args)))
+    (if (every? number? args) (apply f args) (apply mapv f args)))
 
-(defn v+ [& args] (funByElements + args isVector))
+(defn v+ [& args] (funByElements + args isVector?))
 
-(defn v- [& args] (funByElements - args isVector))
+(defn v- [& args] (funByElements - args isVector?))
 
-(defn v* [& args] (funByElements * args isVector))
+(defn v* [& args] (funByElements * args isVector?))
 
 (defn v*s [v & args]
-  {:pre [(isVector v) (every? number? args)]}
+  {:pre [(isVector? v) (every? number? args)]}
     (reduce (fn [v x] (mapv (partial * x) v)) v args))
 
 (defn scalar [a b]
-  {:pre [(isVector a) (isVector b) (checkVectorsEqual [a b])]}
+  {:pre [(isVector? a) (isVector? b) (checkVectorsEqual [a b])]}
     (reduce + (v* a b)))
 
 (defn vect [& args]
-  {:pre [(every? isVector args) (every? (fn [x] (== (count x) 3)) args)]}
+  {:pre [(every? isVector? args) (every? (fn [x] (== (count x) 3)) args)]}
     (reduce (fn [a b] (letfn [(vectCord [x y] (- (* (nth a x) (nth b y)) (* (nth a y) (nth b x))))]
       (vector (vectCord 1 2) (vectCord 2 0) (vectCord 0 1)))) args))
 
-(defn m+ [& args] (funByElements v+ args isMatrix))
+(defn m+ [& args] (funByElements v+ args isMatrix?))
 
-(defn m- [& args] (funByElements v- args isMatrix))
+(defn m- [& args] (funByElements v- args isMatrix?))
 
-(defn m* [& args] (funByElements v* args isMatrix))
+(defn m* [& args] (funByElements v* args isMatrix?))
 
 (defn m*s [m & args]
-  {:pre [(isMatrix m) (every? number? args)]}
+  {:pre [(isMatrix? m) (every? number? args)]}
     (reduce (fn [m s] (mapv (fn [v] (v*s v s)) m)) m args))
 
 (defn m*v [m v]
-  {:pre [(isMatrix m)]}
+  {:pre [(isMatrix? m)]}
     (mapv (fn [a] (reduce + (v* a v))) m))
 
 (defn transpose [m]
-  {:pre [(isMatrix m)]}
+  {:pre [(isMatrix? m)]}
     (apply mapv vector m))
 
 (defn m*m [& args] (reduce (fn [a b] (mapv (partial m*v (transpose b)) a)) args))
 
 (defn broadcast [args]
-  {:post [(fn [args] (if (every? number? args) true (and (checkVectorsEqual args) (recur (mapv first args)))))]}
-  (letfn [(getLevel [t] (if (number? t) 0 (+ (getLevel (first t)) 1)))
-           (getMaxByType [& args] (if (== (count args) 1) (first args)
-            (let [mx (apply getMaxByType (rest args)) mxt (getLevel mx) curt (getLevel (first args))]
-              (if (> mxt curt)
-                mx
-                (first args)))))
-           (castTensor [a b] (if (== (getLevel a) (getLevel b)) a (apply vector (repeat (count b) (castTensor a (first b))))))]
-    (mapv (fn [a] (castTensor a (apply getMaxByType args))) args)))
+  { :pre [(every? isTensor? args)]
+    :post [(fn [args] (if (every? number? args) true (and (checkVectorsEqual args) (recur (mapv first args)))))]}
+    (letfn [(getLevel [t] {:pre [(or (number? t) (vector? t))]} (if (number? t) 0 (+ (getLevel (first t)) 1)))
+             (getMaxByType [& args] (if (== (count args) 1) (first args)
+              (let [mx (apply getMaxByType (rest args)) mxt (getLevel mx) curt (getLevel (first args))]
+                (if (> mxt curt)
+                  mx
+                  (first args)))))
+             (castTensor [a b] (if (== (getLevel a) (getLevel b)) a (apply vector (repeat (count b) (castTensor a (first b))))))]
+      (mapv (fn [a] (castTensor a (apply getMaxByType args))) args)))
 
-(defn tensOp [op tOp args] (if (every? number? args) (funByElements op args identity) (funByElements tOp args identity)))
+(defn tensOp [op tOp args] (if (every? number? args) (funByElements op args isTensor?) (funByElements tOp args isTensor?)))
 
 (defn b+ [& args] (tensOp + b+ (broadcast args)))
 
