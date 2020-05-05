c2a74444f03ccc63b61d6ac29401fdcf5a8e705f
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-05-05 03:01:06 +0300

    fix)

--- ClojureFunctional/expression.clj
+++ ClojureFunctional/expression.clj
@@ -1,3 +1,7 @@
+(defn exp [x] (Math/exp x))
+(defn _sumexp [& args] (apply + (mapv exp args)))
+(defn div [a b] (/ (double a) (double b)))
+
 (defn constant [value] (constantly value))
 
 (defn variable [name] (fn [vars] (get vars name)))
@@ -7,24 +11,43 @@
 (defn unary [op] (fn [x] (fn [vars] (op (x vars)))))
 
 (def add (operation +))
-
 (def subtract (operation -))
-
 (def multiply (operation *))
+(def divide (operation (fn
+                         ([x] (/ x))
+                         ([x & rst] (reduce (fn [a b] (div a b)) x rst)))))
 
-(def divide (operation (fn ([x] (/ (double x))) ([x & rst] (reduce (fn [a b] (/ (double a) (double b))) x rst)))))
+(def sumexp (operation _sumexp))
+(def softmax (operation (fn [& args]
+                          (div (exp (first args)) (apply _sumexp args)))))
 
 (def negate (unary -))
 
-(def symbolToOps {"+" add "-" subtract "*" multiply "/" divide "negate" negate})
+(def symbolToOps {
+                  "+" add
+                  "-" subtract
+                  "*" multiply
+                  "/" divide
+                  "negate" negate
+                  "sumexp" sumexp
+                  "softmax" softmax})
 
 (defn parseFunction [expression]
   (letfn [(parseToken [token]
-          (if (list? token)
-                    (parseOp token)
-                    (if (number? token)
-                      (constant token)
-                      (variable (str token)))))
-          (parseArgs [& argList] (apply vector (parseToken (first argList)) (if (> (count argList) 1) (apply parseArgs (rest argList)) [])))
-          (parseOp [op] (apply (get symbolToOps (str (first op))) (apply parseArgs (rest op))))]
-         (parseToken (read-string expression))))
+            (cond
+             (list? token)
+              (parseOp token)
+             (number? token)
+              (constant token)
+             :else
+              (variable (str token))))
+          (parseArgs [& argList]
+            (apply vector
+                   (parseToken (first argList))
+                   (if (> (count argList) 1)
+                     (apply parseArgs (rest argList))
+                     [])))
+          (parseOp [op]
+                   (apply (get symbolToOps (str (first op)))
+                          (apply parseArgs (rest op))))]
+    (parseToken (read-string expression))))
--- ClojureLinear/linear.clj
+++ ClojureLinear/linear.clj
@@ -4,13 +4,14 @@
 
 (defn isMatrix? [m] (and (vector? m) (every? isVector? m) (checkVectorsEqual m)))
 
-(defn isTensor? [t] (or (every? number? t) (and (checkVectorsEqual t) (every? isTensor? t))))
+(defn isTensor? [t] (or (number? t) (and (checkVectorsEqual t) (every? isTensor? t))))
+
 
 (defn funByElements [f checkArgs]
   (fn [& args]
-    {:pre [(checkVectorsEqual args) (every? checkArgs args)]
+    {:pre [(or (every? number? args) (checkVectorsEqual args)) (every? checkArgs args)]
      :post [(checkArgs %)]}
-      (if (every? number? args) (apply f args) (apply mapv f args))))
+    (if (every? number? args) (apply f args) (apply mapv f args))))
 
 (def v+ (funByElements + isVector?))
 
@@ -43,7 +44,7 @@
 (defn m*s [m & args]
   {:pre [(isMatrix? m) (every? number? args)]
    :post [(isMatrix? %)]}
-    (mapv (fn [v] (apply v* v args)) m))
+    (mapv (fn [v] (apply (partial v*s v) args)) m))
 
 (defn m*v [m v]
   {:pre [(isMatrix? m)]
@@ -61,23 +62,40 @@
     (reduce (fn [a b] (mapv (partial m*v (transpose b)) a)) args))
 
 (defn broadcast [args]
-  { :pre [(every? isTensor? args)]
-    :post [(fn [args] (if (every? number? args) true (and (checkVectorsEqual args) (recur (mapv first args)))))]}
-    (letfn [(getLevel [t] {:pre [(or (number? t) (vector? t))]} (if (number? t) 0 (+ (getLevel (first t)) 1)))
-             (getMaxByType [& args] (if (== (count args) 1) (first args)
-              (let [mx (apply getMaxByType (rest args)) mxt (getLevel mx) curt (getLevel (first args))]
-                (if (> mxt curt)
-                  mx
-                  (first args)))))
-             (castTensor [a b] (if (== (getLevel a) (getLevel b)) a (apply vector (repeat (count b) (castTensor a (first b))))))]
+  {:pre [(every? isTensor? args)]
+   :post [(fn [args]
+            (if (every? number? args)
+              true
+              (and
+               (checkVectorsEqual args)
+               (recur (mapv first args)))))]}
+    (letfn [(getLevel [t]
+                      (if (number? t)
+                        0
+                        (+ (getLevel (first t)) 1)))
+             (getMaxByType [& args]
+                           (if (== (count args) 1)
+                             (first args)
+                              (let [mx (apply getMaxByType (rest args))]
+                                (if (> (getLevel mx) (getLevel (first args)))
+                                  mx
+                                  (first args)))))
+             (castTensor [a b]
+                         (if (== (getLevel a) (getLevel b))
+                           a
+                           (apply vector (repeat (count b) (castTensor a (first b))))))]
       (mapv (fn [a] (castTensor a (apply getMaxByType args))) args)))
 
-(defn tensOp [op tOp] (funByElements (fn [args] (if (every? number? args) (apply op args) (apply tOp args))) isTensor?))
+(defn tensOp [op]
+  (letfn [(recurOp [& args] (if (every? number? args)
+                              (apply op args)
+                              (apply mapv recurOp args)))]
+         (fn [& args] (if (every? number? args) (apply op args) (apply mapv recurOp (broadcast args))))))
 
-(def b+ (tensOp + b+))
+(def b+ (tensOp +))
 
-(def b- (tensOp - b-))
+(def b- (tensOp -))
 
-(def b* (tensOp * b*))
+(def b* (tensOp *))
 
-(def bd (tensOp / bd))
+(def bd (tensOp /))
